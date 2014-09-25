27da43d7da38cc254dbd0cf0ac841a4895ace37c
unknown <guillermo.gallo@GLB6766.globant.com>
2014-09-25 14:17:02 -0300

    adding proyect files

---
+++ app/js/app.js
@@ -0,0 +1,24 @@
+var phonecatApp = angular.module('phonecatApp', [
+//This array lists the modules that phonecatApp depends on:
+'ngRoute',
+'phonecatAnimations',
+'phonecatControllers',
+'phonecatFilters',
+'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+}]);
